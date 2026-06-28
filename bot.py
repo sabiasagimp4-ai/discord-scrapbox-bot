65473b2ac70d09fe334b0bd43d3aaf242edba56f
名前 <ponter1212@gmail.com>
2026-06-28 22:57:56 +0900

    feat: wrap URL in [] for Scrapbox embed

--- bot.py
+++ bot.py
@@ -49,7 +49,7 @@ def fetch_title(url):
 
 def save_to_scrapbox(url, context=''):
     title = fetch_title(url)
-    lines = [title, url]
+    lines = [title, f'[{url}]']
     if context:
         lines.append(f'> {context[:200]}')
     lines.append('from : Discord')
